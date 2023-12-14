7398c14a999f5b885e219c9f395eaa2963530f74
tbutani <tbutani@NO-002-QA.nowonline.server>
2023-12-14 19:33:12 +0530

    Update script-TextProfnCompleteNormalized

--- YounitedAb/src/test/java/apipage/TextkrProfnCompleteNormalized.java
+++ YounitedAb/src/test/java/apipage/TextkrProfnCompleteNormalized.java
@@ -19,13 +19,15 @@ public class TextkrProfnCompleteNormalized {
 			request.header("Content-Type", "application/json");
 			request.header("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlcwYXpDb2U3QjQ1MGszWnBUc2t0RSJ9.eyJuaWNrbmFtZSI6ImxiK2FjYy1hZG1pbiIsIm5hbWUiOiJsYithY2MtYWRtaW5Abm93b25saW5lLm5sIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyLzM3NzNkMjg2MDA4ZTQzMzQ5ZDBmYjk3YzY4YjkzOGMzP3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGbGIucG5nIiwidXBkYXRlZF9hdCI6IjIwMjMtMTItMTRUMTI6NTg6MzQuNTE2WiIsImVtYWlsIjoibGIrYWNjLWFkbWluQG5vd29ubGluZS5ubCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9hY2MteW91bml0ZWQuZXUuYXV0aDAuY29tLyIsImF1ZCI6ImY5UjFVZ2R5SXRKZ2M2NGozMldpZnRwNkRsbGE4WFZvIiwiaWF0IjoxNzAyNTU4NzE5LCJleHAiOjE3MDI3MzE1MTksInN1YiI6ImF1dGgwfDY0OWMyMDZlMTQwNTNmYWI1OGFkZmI0OSIsInNpZCI6Im9oV1dXWEJOSDJsM1Z6b0RTY3Z2R2lVN0YxN0tqN1VBIiwibm9uY2UiOiJNbFZWZGxseE5XdFRVbkppTVRsSVQzTmhXVEJDV2taUmVuVkVVbE5PYkc5WVRINW9kVU5OYWsxU1RnPT0ifQ.llAsmqylG4t_aA0QqP11swxlHBpI3NJIp1yjjG9vJblc27SX4QvhTVMec8ecxL2lVa3GKQ-6C3HpLGDXWWrqqe5E82RVElXDD2ACwCvEVeOPFzRWPMhkz-c5EaK0JI8bg4ckQYl262DWaSn8fAFcb7kFs5SWVxZO_olZV3lUFnnzT4namuQQjATMg46MSh5MVksIZsQ69Lhcqi9JzXxYFSbaykXEV9OgnrVetjjjmQwPaj0dXAujh-3o78ktXjr_cwTWTCUqPa52z8NSPD2g6XFsloEsCOs32Oc5OeF5bjSIx0bboW73UdYHntcFU4tN5v7iYe5ZS2rL176npkSlag");
 			
-			JSONObject json=new JSONObject();
-			json.put("categories", "all");
-			json.put("prefix", "Front");
-			json.put("languages", "nl");
-			json.put("output_language", "nl");
+			//JSONObject json=new JSONObject();
+			//json.put("categories", "all");
+			//json.put("prefix", "Front");
+			//json.put("languages", "nl");
+			//json.put("output_language", "nl");
 			
-			request.body(json.toJSONString());
+			request.body("{ \"categories\": [ \"all\" ], \"prefix\": \"Front\", \"languages\": [ \"nl\" ], \"output_language\": \"nl\" }");
+			
+			//request.body(json.toJSONString());
 			
 			Response response=request.post("https://younited-management-payment-acc.nowonline.nl/CompleteNormalized");
 			
--- YounitedAb/src/test/java/apipage/TextkrSkillsLookup.java
+++ YounitedAb/src/test/java/apipage/TextkrSkillsLookup.java
@@ -1,5 +1,6 @@
 package apipage;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
@@ -18,7 +19,7 @@ public class TextkrSkillsLookup {
 			request.header("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlcwYXpDb2U3QjQ1MGszWnBUc2t0RSJ9.eyJuaWNrbmFtZSI6ImxiK2FjYy1hZG1pbiIsIm5hbWUiOiJsYithY2MtYWRtaW5Abm93b25saW5lLm5sIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyLzM3NzNkMjg2MDA4ZTQzMzQ5ZDBmYjk3YzY4YjkzOGMzP3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGbGIucG5nIiwidXBkYXRlZF9hdCI6IjIwMjMtMTItMTRUMTI6NTg6MzQuNTE2WiIsImVtYWlsIjoibGIrYWNjLWFkbWluQG5vd29ubGluZS5ubCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9hY2MteW91bml0ZWQuZXUuYXV0aDAuY29tLyIsImF1ZCI6ImY5UjFVZ2R5SXRKZ2M2NGozMldpZnRwNkRsbGE4WFZvIiwiaWF0IjoxNzAyNTU4NzE5LCJleHAiOjE3MDI3MzE1MTksInN1YiI6ImF1dGgwfDY0OWMyMDZlMTQwNTNmYWI1OGFkZmI0OSIsInNpZCI6Im9oV1dXWEJOSDJsM1Z6b0RTY3Z2R2lVN0YxN0tqN1VBIiwibm9uY2UiOiJNbFZWZGxseE5XdFRVbkppTVRsSVQzTmhXVEJDV2taUmVuVkVVbE5PYkc5WVRINW9kVU5OYWsxU1RnPT0ifQ.llAsmqylG4t_aA0QqP11swxlHBpI3NJIp1yjjG9vJblc27SX4QvhTVMec8ecxL2lVa3GKQ-6C3HpLGDXWWrqqe5E82RVElXDD2ACwCvEVeOPFzRWPMhkz-c5EaK0JI8bg4ckQYl262DWaSn8fAFcb7kFs5SWVxZO_olZV3lUFnnzT4namuQQjATMg46MSh5MVksIZsQ69Lhcqi9JzXxYFSbaykXEV9OgnrVetjjjmQwPaj0dXAujh-3o78ktXjr_cwTWTCUqPa52z8NSPD2g6XFsloEsCOs32Oc5OeF5bjSIx0bboW73UdYHntcFU4tN5v7iYe5ZS2rL176npkSlag");
 			
 			JSONObject json=new JSONObject();
-			json.put("code_ids", "KS1212F67PN955FY9QGR");
+			/*json.put("code_ids", "KS1212F67PN955FY9QGR");
 			json.put("code_ids", "KS4415Q7473P0DBBD5Y6");
 			json.put("code_ids", "KS123K75YYK8VGH90NCS");
 			json.put("code_ids", "KS1264H6GVR1FW7B62YZ");
@@ -39,9 +40,43 @@ public class TextkrSkillsLookup {
 			json.put("categories", "all");
 			json.put("language", "nl");
 			json.put("output_language", "nl");
-			json.put("threshold", "0");
+			json.put("threshold", "0");*/
 			
-			request.body(json.toJSONString());
+			/*JSONArray codeIdsArray = new JSONArray();
+	        codeIdsArray.add("KS1212F67PN955FY9QGR");
+	        codeIdsArray.add("KS4415Q7473P0DBBD5Y6");
+	        codeIdsArray.add("KS123K75YYK8VGH90NCS");
+	        codeIdsArray.add("KS1264H6GVR1FW7B62YZ");
+	        codeIdsArray.add("KS440FS71DJY51ZPZR0Q");
+	        codeIdsArray.add("KS120ST66K9HBSZ7MPS3");	        
+	        codeIdsArray.add("KS7YXWLEAKN8QOFDFVS2");	        
+	        codeIdsArray.add("KS441BJ6LNS1QCJHRMTW");	        
+	        codeIdsArray.add("KS0MUJGS1UTCN6QBYSB6");	        
+	        codeIdsArray.add("KS128HD6KJSZMPK72WSF");
+	        codeIdsArray.add("KSUOK8Y8NQ2YVBFO4IH5");	        
+	        codeIdsArray.add("KS125JW6MFF4MZ1QZNPP");	        
+	        codeIdsArray.add("KSA8049RVCG293NL3IY2");	        
+	        codeIdsArray.add("KS123RJ5XGVSRR6CH0MS");	        
+	        codeIdsArray.add("KSYD2ESJZLDF3EEES3UY");	        
+	        codeIdsArray.add("KS441BJ71ZQZRPLDVTMB");	        
+	        codeIdsArray.add("KS440W3640PNXPGB5MSW");
+	        codeIdsArray.add("KSVMYR5F3MF8ME0MBN3Q");*/
+			//request.body("{ \"categories\": \"all\", \"languages\": [ \"nl\" ], \"output_language\": \"nl\", \"threshold\": \"0\" }");
+			
+	        String jsonString = "{\"code_ids\": [\"KS1212F67PN955FY9QGR\", \"KS4415Q7473P0DBBD5Y6\", \"KS123K75YYK8VGH90NCS\", \"KS1264H6GVR1FW7B62YZ\", \"KS440FS71DJY51ZPZR0Q\", \"KS120ST66K9HBSZ7MPS3\", \"KS7YXWLEAKN8QOFDFVS2\", \"KS441BJ6LNS1QCJHRMTW\", \"KS0MUJGS1UTCN6QBYSB6\", \"KS128HD6KJSZMPK72WSF\", \"KSUOK8Y8NQ2YVBFO4IH5\", \"KS125JW6MFF4MZ1QZNPP\", \"KSA8049RVCG293NL3IY2\", \"KS123RJ5XGVSRR6CH0MS\", \"KSYD2ESJZLDF3EEES3UY\", \"KS441BJ71ZQZRPLDVTMB\", \"KS440W3640PNXPGB5MSW\", \"KSVMYR5F3MF8ME0MBN3Q\"], \"categories\": [\"all\"], \"language\": \"nl\", \"output_language\": \"nl\", \"threshold\": 0}";
+	        
+	        
+			/*json.put("code_ids", codeIdsArray);
+	        json.put("categories", "Front");
+	        json.put("languages", "nl");
+	        json.put("output_language", "nl");
+	        json.put("threshold", "0");
+	        
+	        String body=json.toJSONString();*/
+			
+	        //System.out.println(body);
+	        
+			request.body(jsonString);
 			
 			Response resp=request.post("https://younited-management-payment-acc.nowonline.nl/SkillsLookup");
 			int scode=resp.getStatusCode();
