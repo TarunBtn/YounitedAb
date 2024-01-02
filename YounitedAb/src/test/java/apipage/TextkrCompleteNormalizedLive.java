87e87bd15ae6beee1f7115092fe0c111c72e030b
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-01-02 14:34:30 +0530

    Update script-CompleteNormalized

---
+++ YounitedAb/src/test/java/apipage/TextkrCompleteNormalizedLive.java
@@ -0,0 +1,40 @@
+package apipage;
+
+import org.json.simple.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
+public class TextkrCompleteNormalizedLive {
+	
+	public void completeNormalized() {
+		
+		RequestSpecification request=RestAssured.given();
+		
+		request.header("Content-Type", "application/json");
+		request.header("APIKey", "052dc90b-9620-4dee-b464-c39b46cc0de4");
+		
+		//JSONObject json=new JSONObject();
+		//json.put("", "")
+		
+		request.body("{\"categories\": [\"all\"], \"prefix\": \"Postman\", \"languages\": [\"nl\"], \"output_language\": \"nl\" }");
+		
+		//request.body(json.toJSONString());
+		
+		Response response=request.post("https://younited-management-payment-pro.nowonline.nl/CompleteNormalized");
+		int statusCode=response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+		System.out.println("Textkr-Status code is-" +statusCode);
+		
+		String data=response.asPrettyString();
+		System.out.println("Response data is- "+data);
+		
+		
+		
+		
+		
+	}
+
+}
