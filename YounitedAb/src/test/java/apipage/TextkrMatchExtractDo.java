33c3b80f04f897674625462942719ce3c8958657
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-01-10 15:53:54 +0530

    Update script-Kvk

--- YounitedAb/src/test/java/apipage/TextkrMatchExtractDo.java
+++ YounitedAb/src/test/java/apipage/TextkrMatchExtractDo.java
@@ -20,7 +20,7 @@ public class TextkrMatchExtractDo {
 	      // Creating request specification
 	      RequestSpecification request = RestAssured.given();
 	                  
-	      request.header("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlcwYXpDb2U3QjQ1MGszWnBUc2t0RSJ9.eyJuaWNrbmFtZSI6ImxiK2FjYy1hZG1pbiIsIm5hbWUiOiJsYithY2MtYWRtaW5Abm93b25saW5lLm5sIiwicGljdHVyZSI6Imh0dHBzOi8vcy5ncmF2YXRhci5jb20vYXZhdGFyLzM3NzNkMjg2MDA4ZTQzMzQ5ZDBmYjk3YzY4YjkzOGMzP3M9NDgwJnI9cGcmZD1odHRwcyUzQSUyRiUyRmNkbi5hdXRoMC5jb20lMkZhdmF0YXJzJTJGbGIucG5nIiwidXBkYXRlZF9hdCI6IjIwMjQtMDEtMTBUMDc6MjQ6MzkuODU1WiIsImVtYWlsIjoibGIrYWNjLWFkbWluQG5vd29ubGluZS5ubCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9hY2MteW91bml0ZWQuZXUuYXV0aDAuY29tLyIsImF1ZCI6ImY5UjFVZ2R5SXRKZ2M2NGozMldpZnRwNkRsbGE4WFZvIiwiaWF0IjoxNzA0ODcxNDg2LCJleHAiOjE3MDUwNDQyODYsInN1YiI6ImF1dGgwfDY0OWMyMDZlMTQwNTNmYWI1OGFkZmI0OSIsImF1dGhfdGltZSI6MTcwNDg3MTQ3OSwic2lkIjoiLWFHX19QYkVBbDdNUDAyNkdSM3d6MVRvd19SLU1MM3MifQ.kBqDUd13tCGzRrIC2xmIvjpICXv34AW8ui0-sy4M6loRWx6Ak595-gdkw0BOifDTct2VHb6NyY_M-eMC26J7fYtrXSR6UpsxPzGXMOFuSmb7_e26JwjlrnU6UxMXZpWUX_6PzbuZRXcPK_yBr-RDKfuizyxmx9WTJBS0Z3MSzW6H8_GeDvYbIBP9grO25FDUgHWAhC5Fe2Qsu9rfZiyKMv5C4W6F-z1p6Nv9Qapr2GK99K5qj4MNN7mBA6DJECy_xwf77f_WzIAKIerSUEbaBJpaSGW2IOn5D7vwO50HSdOpkqD2PBUIL9HJp4Bs2W9Yi0VHm6vaEXf2ce46ALXdKA");
+	      request.header("APIKey", "052dc90b-9620-4dee-b464-c39b46cc0de4");
 	      //request.header("Content-Type", "application/json");
 	      //Adding form data to the request
 	      request.multiPart("publicURL", publicURL);
